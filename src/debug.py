3620bd91d879d4a46462435c2942da1085eaa851
Frank Qin <frank.qjq@outlook.com>
2023-03-02 00:53:05 -0800

    Implement separating music source on selected audio clip.

--- src/debug.py
+++ src/debug.py
@@ -1,5 +1,7 @@
-from models.demucs_plugin import MusicSourceSeparatePlugin
+from plugin import MusicSourceSeparatePlugin
 from tuneflow_devkit import Debugger
+from pathlib import Path
 
 if __name__ == "__main__":
-    Debugger(plugin_class=MusicSourceSeparatePlugin).start()
+    Debugger(plugin_class=MusicSourceSeparatePlugin, bundle_file_path=str(
+        Path(__file__).parent.joinpath('bundle.json').absolute())).start()
---
+++ src/plugin.py
@@ -0,0 +1,88 @@
+from tuneflow_py import TuneflowPlugin, Song, ParamDescriptor, WidgetType, TrackType, InjectSource
+from tuneflow_py.models.protos import song_pb2
+from typing import Any
+import subprocess
+import sys
+from pathlib import Path
+
+class MusicSourceSeparatePlugin(TuneflowPlugin):
+
+    @staticmethod
+    def provider_id():
+        return "qjq"
+
+    @staticmethod
+    def plugin_id():
+        return "separate-track-plugin"
+
+    @staticmethod
+    def params(song: Song) -> dict[str, ParamDescriptor]:
+        return {
+            "selectedClipInfos": {
+                "displayName": {
+                    "zh": '选中片段',
+                    "en": 'Selected Clips',
+                },
+                "defaultValue": None,
+                "widget": {
+                    "type": WidgetType.NoWidget.value,
+                },
+                "hidden": True,
+                "injectFrom": {
+                    "type": InjectSource.SelectedClipInfos.value,
+                    "options": {
+                        "maxNumClips": 1
+                    }
+                }
+            },
+        }
+    
+    @staticmethod
+    def run(song: Song, params: dict[str, Any]):
+        print("=============================")
+        print(
+            "Separating drums, bass, and vocals from the music.")
+        audio_clip_data: song_pb2.AudioClipData = MusicSourceSeparatePlugin._get_selected_audio_clip_data(song, params)
+        MusicSourceSeparatePlugin._separate_music_sources(song, audio_clip_data.audio_file_path, audio_clip_data.duration)
+
+    @staticmethod
+    def _get_selected_audio_clip_data(song: Song, params: dict[str, Any]) -> song_pb2.AudioClipData:
+        selected_clip_infos = params["selectedClipInfos"]
+        selected_clip_info = selected_clip_infos[0]
+        track = song.get_track_by_id(selected_clip_info["trackId"])
+        if track is None:
+            raise Exception("Cannot find track")
+        clip = track.get_clip_by_id(selected_clip_info["clipId"])
+        if clip is None:
+            raise Exception("Cannot find clip")
+        return clip.get_audio_clip_data()
+    
+    @staticmethod
+    def _separate_music_sources(song: Song, audio_file_path, duration):
+        file_path = Path(audio_file_path)
+        if not file_path.exists():
+            raise Exception("Cannot find audio under path: ", audio_file_path)
+        
+        output_dir_path = file_path.parent.joinpath("tuneflow_output")
+
+        print("Start separating music source...")
+        subprocess.run([sys.executable, "-m", "demucs", "-d", "cpu", "--out", output_dir_path.absolute(), audio_file_path], check=True)
+        print("Completed separating music source.")
+
+        output_audio_file_paths = []
+        output_audio_file_paths.append(output_dir_path.joinpath("htdemucs").joinpath(file_path.stem).joinpath("vocals.wav"))
+        output_audio_file_paths.append(output_dir_path.joinpath("htdemucs").joinpath(file_path.stem).joinpath("drums.wav"))
+        output_audio_file_paths.append(output_dir_path.joinpath("htdemucs").joinpath(file_path.stem).joinpath("bass.wav"))
+        output_audio_file_paths.append(output_dir_path.joinpath("htdemucs").joinpath(file_path.stem).joinpath("other.wav"))
+
+        for output_audio_file_path in output_audio_file_paths:
+            MusicSourceSeparatePlugin._create_track(song, str(output_audio_file_path.absolute()), duration) 
+
+    @staticmethod
+    def _create_track(song: Song, audio_file_path, duration):
+        new_track = song.create_track(TrackType.AUDIO_TRACK)
+        new_track.create_audio_clip(clip_start_tick = 0, audio_clip_data = {
+            "audio_file_path": audio_file_path,
+            "start_tick": 0,
+            "duration": duration
+        })
