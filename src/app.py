c1e39f6cc7b08f9c2d9e1231b2b9c0594bf39be3
Frank Qin <frank.qjq@outlook.com>
2023-03-07 22:06:53 -0800

    Add FastAPI server for Demucs plugin.

---
+++ src/app.py
@@ -0,0 +1,6 @@
+from plugin import MusicSourceSeparatePlugin
+from tuneflow_devkit import Runner
+from pathlib import Path
+
+app = Runner(plugin_class_list=[MusicSourceSeparatePlugin], bundle_file_path=str(Path(
+    __file__).parent.joinpath('bundle.json').absolute())).start(path_prefix='/plugins/demucs')
