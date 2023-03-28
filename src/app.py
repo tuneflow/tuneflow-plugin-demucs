c43cd1d778dafa869152f4c4368ef9c183cafaf6
Andantei <zaoyinji@gmail.com>
2023-03-28 14:19:11 -0700

    update plugin icon and app endpoint

--- src/app.py
+++ src/app.py
@@ -3,4 +3,4 @@ from tuneflow_devkit import Runner
 from pathlib import Path
 
 app = Runner(plugin_class_list=[MusicSourceSeparatePlugin], bundle_file_path=str(Path(
-    __file__).parent.joinpath('bundle.json').absolute())).start(path_prefix='/plugins/demucs')
+    __file__).parent.joinpath('bundle.json').absolute())).start(path_prefix='/plugin-service/demucs')
